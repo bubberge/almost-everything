39c038003a71c6a82d6616aba1e23cd054f1b626
David Duffy <daviduffy1@gmail.com>
2016-07-06 17:32:50 -0700

    added styles to content blocks to separate them, added styles to Experience section typography

--- portfolio/incl/_menu.php
+++ portfolio/incl/_menu.php
@@ -1,6 +1,7 @@
-  <ul class="menu vertical" data-magellan>
-    <li><a href="./about.php" data-magellan-target="about">About</a></li>
-    <li><a href="./experience.php" data-magellan-target="experience">Experience</a></li>
-    <li><a href="./portfolio.php" data-magellan-target="portfolio">Portfolio</a></li>
-    <li><a href="./contact.php" data-magellan-target="contact">Contact</a></li>
-  </ul>
+<ul class="menu vertical" data-magellan>
+    <li><a href="#splash" data-magellan-target="splash">Top</a></li>
+    <li><a href="#about" data-magellan-target="about">About</a></li>
+    <li><a href="#experience" data-magellan-target="experience">Experience</a></li>
+    <li><a href="#portfolio" data-magellan-target="portfolio">Portfolio</a></li>
+    <li><a href="#contact" data-magellan-target="contact">Contact</a></li>
+</ul>

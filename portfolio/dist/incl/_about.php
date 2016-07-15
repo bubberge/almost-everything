1559bf46b1ae0e8544bfc027ac6f0de5b7d9837a
David Duffy <daviduffy1@gmail.com>
2016-07-14 18:32:47 -0700

    added clean task, added sourcemaps for js and css/scss, added watch task, updated build to include clean task. Dist folder now includes everything for production after build.

---
+++ portfolio/dist/incl/_about.php
@@ -0,0 +1,4 @@
+<div id="about" class="about-content fade-in fade-in-down">
+  <h1>About</h1>
+  <p>David is an experienced, self-motivated problem solver and leader. He is laser-focused and analytical, able to navigate through challenges and ambiguity with poise. He communicates with clarity and empathy, and has a track record of building trusting, positive, and lasting relationships with team members, supervisors, and clientele. He is intelligent and determined, able to analyze obstacles, quickly learn and apply new skills, and rise to the occasion.</p>   
+</div>

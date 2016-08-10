cbf49016fa0f1dbbc15410e0708473e8d949119b
bubberge <david.duffy@crystalcommerce.com>
2016-08-10 09:27:57 -0700

    updated monospace font to be a google font so that presentation is the same on all devices. changed title attributes in the circleGen area

--- portfolio/dist/incl/_circlediv.php
+++ portfolio/dist/incl/_circlediv.php
@@ -1,4 +1,4 @@
-<div class='row show-for-medium circleGen' title='A little JavaScript doodle. On mouseover (hover), large circles visually split into smaller circles. It is difficult to split and re-assemble the circles because there is some kind of issue with the re-assembly function that occasionally causes the circles to join prematurely.'>
+<div class='row show-for-medium circleGen'>
     <div class='medium-2 column circleGen-controls'>
         <h2><a class='fancy-link' href='http://jsbin.com/fudizi/edit?html,js,output' title='link to the JSBin for this doodle'>circleGen<span id='inReverse' class='hide'>Reverse</span>()&#59;</a></h2>
         <p class='created'>Created
@@ -19,7 +19,7 @@
         <a href='JavaScript:void(0)' class='reset control-button' title='resets the circleDiv doodle to its original state'>Reset</a>
     </div>
     <div class='medium-8 medium-pull-2 column'>
-        <div class='circleGen-container'>
+        <div class='circleGen-container' title='A gamified JavaScript doodle. On mouseover (hover), large circles visually split into smaller circles. Reverse the puzzle and put all of the circles back together before the timer ends.'>
         <span class="circleGen-overlay">
             <span>Game Over</span>
             <a href='JavaScript:void(0)' class='reset control-button' title='resets the circleDiv doodle to its original state'>Reset</a>

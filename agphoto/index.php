3dda03a5dc73af98d697ab442328408054eb3f5e
daviduffy@hotmail.com <david.duffy@crystalcommerce.com>
2017-03-01 23:22:17 -0800

    updated markup to use new html5 picture element for homepage. had to change CSS to adapt to the new homepage slideshow implementation. changing those styles made updates to all other pages splash areas. still performing adjustments and cleanup

--- agphoto/index.php
+++ agphoto/index.php
@@ -10,29 +10,173 @@ include 'incl/_header.php'; ?>
             <div class='uil-ring-css' style='transform:scale(0.45);'><div></div></div>
         </div>
         <div class="j-track">
-            <div class="j-background j-1 rm-rf">
-                <img src="img/homepage/001-leavenworth_wedding_photographer-betz-001-small.jpg" srcset="img/homepage/001-leavenworth_wedding_photographer-betz-001.jpg 2000w" sizes="(orientation: landscape) 100vw, 145vh" alt="" />
+            <div class="j-background j-1">
+
+                <picture>
+
+                    <source srcset="img/homepage/h1334.png" media="(orientation: portrait) and (min-width: 500px)" >
+                    <source srcset="img/homepage/h667.png" media="(orientation: portrait)"  >
+
+
+                    <source srcset="img/homepage/2000.png" media="(min-width: 1440px)" />
+                    <source srcset="img/homepage/1440.png" media="(min-width: 1024px)" />
+                    <source srcset="img/homepage/1024.png" media="(min-width: 768px)" />
+                    <source srcset="img/homepage/768.png"/>
+                    <img src="img/homepage/1024.png" alt="testing"/>
+
+<!--
+                    <source media="(orientation: portrait)" srcset="img/homepage/h667/001-leavenworth_wedding_photographer-betz-001-667h.jpg 1x, img/homepage/h1334/001-leavenworth_wedding_photographer-betz-001-1334h.jpg 2x" >
+                    <source media="(orientation: portrait) and (min-width: 500px)" src="img/homepage/h1334/001-leavenworth_wedding_photographer-betz-001-1334h.jpg" >
+
+
+                    <source srcset="img/homepage/w2000/001-leavenworth_wedding_photographer-betz-001-2000.jpg" media="(min-width: 1440px)" />
+                    <source srcset="img/homepage/w1440/001-leavenworth_wedding_photographer-betz-001-1440.jpg" media="(min-width: 1024px)" />
+                    <source srcset="img/homepage/w1024/001-leavenworth_wedding_photographer-betz-001-1024.jpg" media="(min-width: 768px)" />
+                    <source srcset="img/homepage/w768/001-leavenworth_wedding_photographer-betz-001-768.jpg"/>
+                    <img src="img/homepage/w1024/001-leavenworth_wedding_photographer-betz-001-1024.jpg" alt="Bride and groom kiss in front of a moody mountain sky on the shore of scenic Lake Wenatchee"/> -->
+
+
+                </picture>
+
             </div>
+            <div class="j-background j-2">
+
+                <picture>
+
+                    <!-- portrait images -->
+                    <source media="(orientation: portrait) and (min-width: 500px)" srcset="img/homepage/h1334/002-jackson_hole_wedding_photographer-todd-002-1334h.jpg" >
+                    <source media="(orientation: portrait)" srcset="img/homepage/h667/002-jackson_hole_wedding_photographer-todd-002-667h.jpg" >
+
+                    <!-- landscape images -->
+                    <source srcset="img/homepage/w2000/002-jackson_hole_wedding_photographer-todd-002-2000.jpg" media="(min-width: 1440px)" />
+                    <source srcset="img/homepage/w1440/002-jackson_hole_wedding_photographer-todd-002-1440.jpg" media="(min-width: 1024px)" />
+                    <source srcset="img/homepage/w1024/002-jackson_hole_wedding_photographer-todd-002-1024.jpg" media="(min-width: 768px)" />
+                    <source srcset="img/homepage/w768/002-jackson_hole_wedding_photographer-todd-002-768.jpg"/>
+                    <img src="img/homepage/w1024/002-jackson_hole_wedding_photographer-todd-002-1024.jpg" alt="mountain bride and groom sit on a chair lift in jackson hole, wyoming"/>
+                    <p><!-- mountain bride and groom sit on a chair lift in jackson hole, wyoming --></p>
+
+                </picture>
+
+            </div>
+            <!--
             <div class="j-background j-2">
                 <img src="img/homepage/002-jackson_hole_wedding_photographer-todd-002-small.jpg" srcset="img/homepage/002-jackson_hole_wedding_photographer-todd-002.jpg 2000w" sizes="(orientation: landscape) 100vw, 145vh" alt="" />
             </div>
+            -->
             <div class="j-background j-3">
-                <img src="img/homepage/002-wisconsin-wedding-photographer-001-small.jpg" srcset="img/homepage/002-wisconsin-wedding-photographer-001.jpg 2000w" sizes="(orientation: landscape) 100vw, 145vh" alt="" />
+
+                <picture>
+
+                    <!-- portrait images -->
+                    <source media="(orientation: portrait) and (min-width: 500px)" srcset="img/homepage/h1334/002-wisconsin-wedding-photographer-001-1334h.jpg" >
+                    <source media="(orientation: portrait)" srcset="img/homepage/h667/002-wisconsin-wedding-photographer-001-667h.jpg" >
+
+
+                    <!-- landscape images -->
+                    <source srcset="img/homepage/w2000/002-wisconsin-wedding-photographer-001-2000.jpg" media="(min-width: 1440px)" />
+                    <source srcset="img/homepage/w1440/002-wisconsin-wedding-photographer-001-1440.jpg" media="(min-width: 1024px)" />
+                    <source srcset="img/homepage/w1024/002-wisconsin-wedding-photographer-001-1024.jpg" media="(min-width: 768px)" />
+                    <source srcset="img/homepage/w768/002-wisconsin-wedding-photographer-001-768.jpg"/>
+                    <img src="img/homepage/w1024/002-wisconsin-wedding-photographer-001-1024.jpg" alt=""/>
+                    <p><!--  --></p>
+
+                </picture>
+
             </div>
-            <div class="j-background j-4 rm-rf">
-                <img src="img/homepage/003-leavenworth_wedding_photographer-jasinski-004-small.jpg" srcset="img/homepage/003-leavenworth_wedding_photographer-jasinski-004.jpg 2000w" sizes="(orientation: landscape) 100vw, 145vh" alt="" />
+            <div class="j-background j-4">
+
+                <picture>
+
+                    <!-- portrait images -->
+                    <source class="portrait" media="(orientation: portrait) and (min-width:500px)" srcset="img/homepage/h1334/003-leavenworth_wedding_photographer-jasinski-004-1334h.jpg" >
+                    <source class="portrait" media="(orientation: portrait)" srcset="img/homepage/h667/003-leavenworth_wedding_photographer-jasinski-004-667h.jpg" >
+
+                    <!-- landscape images -->
+                    <source srcset="img/homepage/w2000/003-leavenworth_wedding_photographer-jasinski-004-2000.jpg" media="(min-width: 1440px)" />
+                    <source srcset="img/homepage/w1440/003-leavenworth_wedding_photographer-jasinski-004-1440.jpg" media="(min-width: 1024px)" />
+                    <source srcset="img/homepage/w1024/003-leavenworth_wedding_photographer-jasinski-004-1024.jpg" media="(min-width: 768px)" />
+                    <source srcset="img/homepage/w768/003-leavenworth_wedding_photographer-jasinski-004-768.jpg"/>
+                    <img src="img/homepage/w1024/003-leavenworth_wedding_photographer-jasinski-004-1024.jpg" alt=""/>
+                    <p><!--  --></p>
+
+                </picture>
+
             </div>
             <div class="j-background j-5">
-                <img src="img/homepage/004-within-sodo-wedding-aebischer-small.jpg" srcset="img/homepage/004-within-sodo-wedding-aebischer.jpg 2000w" sizes="(orientation: landscape) 100vw, 145vh" alt="" />
+
+                <picture>
+
+                    <!-- portrait images -->
+                    <source media="(orientation: portrait) and (min-width:500px)" srcset="img/homepage/h1334/004-within-sodo-wedding-aebischer-1334h.jpg" >
+                    <source media="(orientation: portrait)" srcset="img/homepage/h667/004-within-sodo-wedding-aebischer-667h.jpg" >
+
+                    <!-- landscape images -->
+                    <source srcset="img/homepage/w2000/004-within-sodo-wedding-aebischer-2000.jpg" media="(min-width: 1440px)" />
+                    <source srcset="img/homepage/w1440/004-within-sodo-wedding-aebischer-1440.jpg" media="(min-width: 1024px)" />
+                    <source srcset="img/homepage/w1024/004-within-sodo-wedding-aebischer-1024.jpg" media="(min-width: 768px)" />
+                    <source srcset="img/homepage/w768/004-within-sodo-wedding-aebischer-768.jpg"/>
+                    <img src="img/homepage/w1024/004-within-sodo-wedding-aebischer-1024.jpg" alt=""/>
+                    <p><!--  --></p>
+
+                </picture>
+
             </div>
             <div class="j-background j-6">
-                <img src="img/homepage/006-mt-baker-engagement-session-001-small.jpg" srcset="img/homepage/006-mt-baker-engagement-session-001.jpg 2000w" sizes="(orientation: landscape) 100vw, 145vh" alt="" />
+
+                <picture>
+
+                    <!-- portrait images -->
+                    <source media="(orientation: portrait) and (min-width:500px)" srcset="img/homepage/h1334/006-mt-baker-engagement-session-001-1334h.jpg" >
+                    <source media="(orientation: portrait)" srcset="img/homepage/h667/006-mt-baker-engagement-session-001-667h.jpg" >
+
+                    <!-- landscape images -->
+                    <source srcset="img/homepage/w2000/006-mt-baker-engagement-session-001-2000.jpg" media="(min-width: 1440px)" />
+                    <source srcset="img/homepage/w1440/006-mt-baker-engagement-session-001-1440.jpg" media="(min-width: 1024px)" />
+                    <source srcset="img/homepage/w1024/006-mt-baker-engagement-session-001-1024.jpg" media="(min-width: 768px)" />
+                    <source srcset="img/homepage/w768/006-mt-baker-engagement-session-001-768.jpg"/>
+                    <img src="img/homepage/w1024/006-mt-baker-engagement-session-001-1024.jpg" alt=""/>
+                    <p><!--  --></p>
+
+                </picture>
+
             </div>
-            <div class="j-background j-7 rm-rf">
-                <img src="img/homepage/007-bainbridge-island-engagement-001-small.jpg" srcset="img/homepage/007-bainbridge-island-engagement-001.jpg 2000w" sizes="(orientation: landscape) 100vw, 145vh" alt="" />
+            <div class="j-background j-7">
+
+                <picture>
+
+                    <!-- portrait images -->
+                    <source media="(orientation: portrait) and (min-width:500px)" srcset="img/homepage/h1334/007-bainbridge-island-engagement-001-1334h.jpg" >
+                    <source media="(orientation: portrait)" srcset="img/homepage/h667/007-bainbridge-island-engagement-001-667h.jpg" >
+
+                    <!-- landscape images -->
+                    <source srcset="img/homepage/w2000/007-bainbridge-island-engagement-001-2000.jpg" media="(min-width: 1440px)" />
+                    <source srcset="img/homepage/w1440/007-bainbridge-island-engagement-001-1440.jpg" media="(min-width: 1024px)" />
+                    <source srcset="img/homepage/w1024/007-bainbridge-island-engagement-001-1024.jpg" media="(min-width: 768px)" />
+                    <source srcset="img/homepage/w768/007-bainbridge-island-engagement-001-768.jpg"/>
+                    <img src="img/homepage/w1024/007-bainbridge-island-engagement-001-1024.jpg" alt=""/>
+                    <p><!--  --></p>
+
+                </picture>
+
             </div>
-            <div class="j-background j-8 rm-rf">
-                <img src="img/homepage/008-stevens-pass-engagement-session-001-small.jpg" srcset="img/homepage/008-stevens-pass-engagement-session-001.jpg 2000w" sizes="(orientation: landscape) 100vw, 145vh" alt="" />
+            <div class="j-background j-8">
+
+                <picture>
+
+                    <!-- portrait images -->
+                    <source media="(orientation: portrait) and (min-width:500px)" srcset="img/homepage/h1334/008-stevens-pass-engagement-session-001-1334h.jpg" >
+                    <source media="(orientation: portrait)" srcset="img/homepage/h667/008-stevens-pass-engagement-session-001-667h.jpg" >
+
+                    <!-- landscape images -->
+                    <source srcset="img/homepage/w2000/008-stevens-pass-engagement-session-001-2000.jpg" media="(min-width: 1440px)" />
+                    <source srcset="img/homepage/w1440/008-stevens-pass-engagement-session-001-1440.jpg" media="(min-width: 1024px)" />
+                    <source srcset="img/homepage/w1024/008-stevens-pass-engagement-session-001-1024.jpg" media="(min-width: 768px)" />
+                    <source srcset="img/homepage/w768/008-stevens-pass-engagement-session-001-768.jpg"/>
+                    <img src="img/homepage/w1024/008-stevens-pass-engagement-session-001-1024.jpg" alt=""/>
+                    <p><!--  --></p>
+
+                </picture>
+
             </div>
         </div>
         <div class="j-overlay">
--- agphoto/js/app.js
+++ agphoto/js/app.js
@@ -16,9 +16,6 @@ if ( $slickHome[0] ) {
 function slickHome(){ // removes images that aren't good for small and then inits slick on home
     console.log('¸.·´¯`·.´¯`·.¸¸.·´¯`·.¸><(((º>');
     var $jumbo = $('.j-background');
-    if ( $slickHome[0] && $jumbo.width() < 640 ) {
-        $('.rm-rf').remove();
-    }
     $('.j-track').slick({
         dots: true,
         infinite: true,

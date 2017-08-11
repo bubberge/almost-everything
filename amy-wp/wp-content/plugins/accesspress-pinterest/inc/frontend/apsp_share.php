6841a86d71f60e274a8db3c127c19509a9d9f031
David Duffy <daviduffy1@gmail.com>
2017-08-10 22:08:14 -0700

    removed full wordpress install from repo, keeping theme only

--- amy-wp/wp-content/plugins/accesspress-pinterest/inc/frontend/apsp_share.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('ABSPATH') or die("No script kiddies please!");
-?>
-<div class='apsp-share'>
-	<a href='javascript:pinIt();'><?php _e('Pin it', 'accesspress-pinterest'); ?></a>
-</div>
--- amy-wp/wp-content/plugins/accesspress-pinterest/inc/frontend/pin-widget-shortcode.php
+++
@@ -1,4 +0,0 @@
-<?php defined('ABSPATH') or die("No script kiddies please!");
-$atts_image = isset($atts['image_url']) ? $atts['image_url'] : 'https://www.pinterest.com/pin/434034482809764694/';
-?>
-<a data-pin-do="embedPin" href="<?php echo $atts_image; ?>"></a>

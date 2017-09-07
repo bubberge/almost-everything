322660806a9a2120818082c3595f51cd9f40437e
David Duffy <daviduffy1@gmail.com>
2017-09-06 19:12:08 -0700

    pairing away important sites from this slush repo

--- agphoto-blog-theme/includes/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="large-3 show-for-large-up columns">
-	
-	<?php if ( is_active_sidebar( 'home_right_1' ) ) : ?>
-		<div id="amy-widgets" class="navigation" role="complementary">
-			<?php dynamic_sidebar( 'home_right_1' ); ?>
-		</div><!-- #amy-widgets -->
-	<?php endif; ?>
-
-</div>

2c3b0d5c79bcaad0d58996a8463e7dae63cc2c6f
David Duffy <daviduffy1@gmail.com>
2018-03-23 12:44:51 -0700

    updated issues with porfolio site copy and inability to click links

--- portfolio/incl/_about.php
+++ portfolio/incl/_about.php
@@ -56,18 +56,19 @@
   <div class="small-9 column scrollContent CC" style="background-color: #fff; position: relative; z-index: 2;">
     <h2>CrystalCommerce</h2>
     <span class="year show-for-medium">2014-<?php echo date("Y"); ?></span>
-    <p>Started as a customer service rep. Worked hard, moved into web design and UX, and then into product management.</p>
+    <p>Started as a customer service rep. Worked hard, moved into web design and UX, into product management, and finally settled in Frontend Development. My journey has taken me through myriad product develpment disciplines, and helped me build the experience to be a powerful contributor.</p>
     <span><span>Customer Service:</span> refined that ever-important ability to explain technical subject matter to non-technical persons</span>
     <span><span>Design/Development:</span> went from 4th-grade reading level to fluent in HTML/SCSS and can do work with JS as well</span>
     <span><span>Product Management:</span> learned to build consensus, to adapt to uncertainty, and a lot about agile process</span>
+    <span><span>Frontend Development:</span> learned to work with old and new technologies and apply all my previous experience to build creative, awesome products</span>
   </div>
 
   <div class="small-3 column scrollFill omega" style="background-color: #fff; position: relative; z-index: 2;">
       <img src="img/ME.png">
   </div>
-  <div class="small-9 column scrollContent ME" style="background-color: #fff; position: relative; z-index: 2;"contenteditable="">
+  <div class="small-9 column scrollContent ME" style="background-color: #fff; position: relative; z-index: 2;">
     <h2>Outside Work</h2>
-    <p>I get pretty bored if I'm not challenging myself with something new and difficult to focus on. The things I am proudest of all follow that mold.</p>
+    <p>I am always on the lookout for something new and difficult to focus on. The things I am proudest of all follow that mold.</p>
     <span><span>Snowboarding:</span>In 2009 I moved to Jackson, WY just to snowboard. Inadvertently, the jobs that I had there laid the groundwork for pursuing web develoment.</span>
     <span><span>Halo Suit:</span>I think armor costumes are cool. Like a true DIY enthusiast, I watched about 2 minutes of a YouTube video and figured <br class="show-for-medium">I could make one too. <a target="_blank" href="http://imgur.com/a/D7oUQ#0" title="photo gallery [imgur] of my self-made Halo 3 MJOLNIR Mark VI suit">It went better than expected.</a></span>
     <span><span>Web Develoment:</span>The world of webdev is basically endless, and it can be self-taught. It may seem strange because web development is just work for so many people, but for me it's a fun world to explore.</span>
